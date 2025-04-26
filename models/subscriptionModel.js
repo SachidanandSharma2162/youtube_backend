3c546edb3d042b158851beb74d4a111505368dbb
sachi_2162 <sharmasachidanand1111@gmail.com>
2025-04-26 21:33:38 +0530

    added subscriber model

--- models/subscriptionModel.js
+++ models/subscriptionModel.js
@@ -1,12 +1,12 @@
 const mongoose=require("mongoose")
 
 const subscriberSchema=({
-    subscriber:[{
-        type:mongoose.Schema.Types.ObjectId,
+    subscriber:{
+        type:mongoose.Schema.Types.ObjectId, // one who is subscribing
         ref:"User"
-    }],
+    },
     channel:{
-        type:mongoose.Schema.Types.ObjectId,
+        type:mongoose.Schema.Types.ObjectId, // one who is subscribed
         ref:"User"
     }
 },{
